15fde2b60a35ece9d8d010305dd1dc770b46b558
mephistachio <47401980+mephistachio@users.noreply.github.com>
2025-07-24 15:48:18 +0200

    Update java_task_1.java

--- java_task_1.java
+++ java_task_1.java
@@ -8,3 +8,21 @@ After each student is registered, the code stub requests and prints the student'
 Constraints 1 snumberOfStudents is 10 
 Input Format For Custom Testing 
 The first line contains the value of numberOfStudents that describes the total number of students being registered. Each of the next numberOfStudents lines contains the student name */
+
+import java.util.Scanner;
+
+public class Solution {
+    private static final Scanner INPUT_READER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int numberOfStudents = Integer.parseInt(INPUT_READER.nextLine());
+
+        while (numberOfStudents-- > 0) {
+            String studentName = INPUT_READER.nextLine();
+            Student student = new Student(studentName);
+            System.out.println(student.toString());
+        }
+
+        INPUT_READER.close();
+    }
+}
