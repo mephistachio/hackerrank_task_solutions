8b7a52469a670e7045683a238436f78f450f0318
mephistachio <47401980+mephistachio@users.noreply.github.com>
2025-07-24 15:47:07 +0200

    Create java_task_1.java

---
+++ java_task_1.java
@@ -0,0 +1,10 @@
+/*Java: 
+The Student Class Implement a student registration and student information retrieval system for a school using a simple class roster in Java. 
+When a student is registered, the system must assign an integer ID (enrollment Number), starting at 1 and adding 1 as each student is registered. 
+The student's name is stored with the assigned enrollmentNumber. The retrieval request should return a student's registration information. 
+The Student class should implement: The constructor. Student(String name) The method String toString() to return the string" (enrollmentNumber): (name) 
+The locked stub code in the editor validates the implementation of the Student class. 
+After each student is registered, the code stub requests and prints the student's information to test your code. 
+Constraints 1 snumberOfStudents is 10 
+Input Format For Custom Testing 
+The first line contains the value of numberOfStudents that describes the total number of students being registered. Each of the next numberOfStudents lines contains the student name */
